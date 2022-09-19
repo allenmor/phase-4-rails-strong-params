bfa0743a8ac4e07a3612a39edbeb8f896bc79155
Allen Mordukhaev <allenmor93@gmail.com>
2022-09-19 15:21:08 -0400

    commit tree 0c15496c479e07e385fec75afea1ac58fc906805

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,7 +8,10 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(
+      name: params[:name], 
+      species: params[:species]
+    )
     render json: bird, status: :created
   end
 
